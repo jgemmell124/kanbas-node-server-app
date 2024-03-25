bc65681d7d45b6e24ef3e4340d81089e02b14b05
john <johngemmell124@gmail.com>
2024-03-25 17:55:14 -0400

    part 3 done lab 6

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import express from 'express'
+import mogoose from 'mongoose'
 import cors from 'cors'
 import Hello from './Hello.js'
 import Lab5 from './Lab5.js'
 import CourseRoutes from './Kanbas/courses/routes.js'
 import ModuleRoutes from './Kanbas/modules/routes.js'
+import UserRoutes from './users/routes.js'
+
+mogoose.connect('mongodb://localhost:27017/kanbas');
 
 const PORT = process.env.PORT || 4000;
 
@@ -12,6 +16,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 
